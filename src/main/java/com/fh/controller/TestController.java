651630be98fef53e25cd5f232516081733d33021
bbj0916 <3103242361@qq.com>
2020-11-20 11:19:36 +0800

    第2次提交

--- src/main/java/com/fh/controller/TestController.java
+++ src/main/java/com/fh/controller/TestController.java
@@ -11,6 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @RequestMapping("test")
     public String test(){
-        return "success";
+        return "success11111";
     }
 }
