1f282abe9ef3ea80daaa340b4c47113428e49423
bbj0916 <3103242361@qq.com>
2020-11-20 11:04:18 +0800

    第一次提交

---
+++ src/main/java/com/fh/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.fh.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author huangp
+ * @create 2020-11-19 15:49
+ */
+@RestController
+public class TestController {
+    @RequestMapping("test")
+    public String test(){
+        return "success";
+    }
+}
